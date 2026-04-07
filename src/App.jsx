9e5a4fa0107af43eb350b840ddaa169d4821d403
laurabrilhantee <laurabrilhante8@gmail.com>
2026-04-07 16:11:21 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,51 @@
 import { useState } from 'react'
 import './App.css'
 import Ausie from './assets/A.png'
-import ribs from './assets/Ribs.jpg'
-import picanha from './assets/Picanha.jpg'
-import outbacker from './assets/Outbacker.jpg'
+import Ribs from './assets/Ribs.jpg'
+import Picanha from './assets/Picanha.jpg'
+import Outbacker from './assets/Outbacker.jpg'
+import logo from './assets/image.png'
 
 export default function App() {
 
   const CLASSES = [
-    { nome: "Picanha Bloomin’ Burger", emoji: "picanha"},
-    { nome: "Ribs Double Decker", emoji: "ribs"},
-    { nome: "Aussie Plant Burger", emoji: "ausie"},
-    { nome: "The Outbacker Burger", emoji: "outbacker"},
+    { nome: "Picanha Bloomin’ Burger", img: Picanha},
+    { nome: "Ribs Double Decker", img: Ribs},
+    { nome: "Aussie Plant Burger", img: Ausie},
+    { nome: "The Outbacker Burger", img: Outbacker},
   ];
+
   //Variaveis useStates
-  const [nome, setNome] = useState(""); //texto
-  const [hp, setHp] = useState(100); //número
-  const [vivo, setVivo] = useState(true); //boolean
-  const [classe, setClasse] = useState(CLASSES[0]); //objeto
+  const [nome, setNome] = useState(""); // nome do pedido
+  const [hp, setHp] = useState(100); // satisfação do cliente
+  const [vivo, setVivo] = useState(true); // pedido disponível
+  const [classe, setClasse] = useState(CLASSES[0]); // lanche selecionado
 
   const receberDano = () => {
-    const novoHp = Math.max(0, hp-10);
-    setHp(novoHp);
-    setVivo(novoHp > 0);
-  }
+  const novoHp = Math.max(0, hp - 10);
+  setHp(novoHp);
+  setVivo(novoHp > 0);
+}
 
-  const curar = () => {
-    setHp(100);
-    setVivo(true);
-  }
+const curar = () => {
+  setHp(100);
+  setVivo(true);
+}
 
-  const pct = hp/100;
-  const corBarra = pct > 0.5 ? "#5DCAA5" : pct > 0.25 ? "#EF9F27" : "#E24B4A";
+const pct = hp / 100;
+const corBarra = pct > 0.5 ? "#5DCAA5" : pct > 0.25 ? "#EF9F27" : "#E24B4A";
   
   return (
     <>
       <main>
         <section>
           
-          <h1>Outback</h1>
+          <h1>
+            <img src="/src/assets/image.png" className="logo" />
+          </h1>
+        
           <div className="thumb">
-            <img src={CLASSES.classe} alt="" />
+            <img src={classe.img} alt={classe.nome} />
           </div>
           <input 
             type="text" 
@@ -50,33 +55,42 @@ export default function App() {
             onChange={(e) => setNome(e.target.value)}  
           />
 
-          <div className="status">
-            <p>Status</p>
-            <p>Vivo (boolean)</p>
-            <span>{vivo ? "VIVO" : "MORTO"}</span>
-            <span>{vivo ? "true" : "false"}</span>
-          </div>
+             {/* STATUS DO PEDIDO */}
+        <div className="status">
+          <p>Status do Pedido</p>
+          <p>Disponível</p>
+          <span>{vivo ? "DISPONÍVEL" : "INDISPONÍVEL"}</span>
+          <span>{vivo ? "true" : "false"}</span>
+        </div>
 
-          <p id='pontosVida'>Pontos de vida (HP) {hp}/100</p>
-          <div className="barra" style={{background: corBarra}}>
-          </div>
+        {/* SATISFAÇÃO DO CLIENTE */}
+        <p id='pontosVida'>
+          Satisfação do Cliente {hp}/100
+        </p>
 
-          <button 
-            className='BTcura'
-            onClick={receberDano}  
-            disabled={!vivo}
-          >Receber Dano</button>
-          
-          <button 
-            className='BTcura'
-            onClick={curar} 
-          >Curar</button>
-          
+        <div className="barra" style={{background: corBarra}}></div>
+
+        {/* AÇÕES */}
+        <button 
+          className='BTcura'
+          onClick={receberDano}  
+          disabled={!vivo}
+        >
+          Reclamação 
+        </button>
+        
+        <button 
+          className='BTcura'
+          onClick={curar} 
+        >
+          Elogio
+        </button>
+        
           <div className="classes">
-            <button id='picanha' onClick={() => setClasse(CLASSES[0])}>Picanha Bloomin’ Burger</button>
-            <button id='ribs' onClick={() => setClasse(CLASSES[1])}> Ribs Double Decker</button>
-            <button id='aussie' onClick={() => setClasse(CLASSES[2])}> Aussie Plant Burger</button>
-            <button id='outbacker' onClick={() => setClasse(CLASSES[3])}> The Outbacker Burger </button>
+            <button id='Picanha' onClick={() => setClasse(CLASSES[0])}>Picanha Bloomin’ Burger</button>
+            <button id='Ribs' onClick={() => setClasse(CLASSES[1])}> Ribs Double Decker</button>
+            <button id='Aussie' onClick={() => setClasse(CLASSES[2])}> Aussie Plant Burger</button>
+            <button id='Outbacker' onClick={() => setClasse(CLASSES[3])}> The Outbacker Burger </button>
           </div>
 
         </section>
